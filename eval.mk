47ff9a85f92e9655dcea656de9646b3cf2abe6b0
Laiagdla <25137442+Laiagdla@users.noreply.github.com>
2025-07-04 16:23:18 +0200

    evaluation commands

---
+++ eval.mk
@@ -0,0 +1,25 @@
+graphic-interface:
+	ls /usr/bin/*session
+
+ufw:
+	sudo ufw status
+	sudo service ufw status
+
+# sudo systemctl status ssh
+
+# uname --kernel-version
+
+# getent group sudo user42
+
+# sudo adduser name_user
+# sudo addgroup evaluating
+# sudo adduser name_user evaluating
+
+# hostname
+
+# sudo nano /etc/hostname
+# sudo nano /etc/hosts
+
+# sudo reboot
+
+# lsblk
